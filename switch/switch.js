2cd057c0cb2c96ce16e4e25a2a918ed39c8e71d3
Bobbymck <bmckinn7@stumail.jccc.edu>
2026-07-05 14:50:17 -0500

    Add files via upload

---
+++ switch/switch.js
@@ -0,0 +1,31 @@
+//Bobby McKinney 7/5/26
+
+let favMonth = prompt('What is your favorite month?').toLowerCase();
+
+switch(favMonth) {
+	case 'march':
+	case 'april':
+	case 'may':
+		alert('"Spring is nice with everything blooming."');
+		break;
+	
+	case 'june':
+	case 'july':
+	case 'august':
+		alert('"You enjoy the summer months!"');
+		break;
+	
+	case 'september':
+	case 'october':
+	case 'november':
+		alert('"Fall is fun with all of the pretty colors."');
+		break;
+	
+	case 'december':
+	case 'january':
+	case 'february':
+		alert('"You love the winter months!"');
+		break;
+	default:
+		alert('"Other months are interesting too!"');
+}
